4e3de67d93a15f20ac92758367a88e65d66be6e2
Delta <maxenot123@gmail.com>
2025-09-28 21:39:38 +0200

    add test table

--- lab2/solution_test.go
+++ lab2/solution_test.go
@@ -6,17 +6,37 @@ import (
 
 func TestCalc(t *testing.T) {
 	// arrange
-	s := "(21+33)*3.2-4+(5*2-6)*2="
-	exp := 176.8
+	var tests = []struct {
+		name    string
+		s       string
+		want    float64
+		wantErr bool
+	}{
+		{"normal_1", "(21+33)*3.2-4+(5*2-6)*2=", 176.8, false},
+		{"empty_string", "", 0.0, true},
+		{"only_equal_sign", "=", 0.0, false},
+		{"random_text_with_equal_sign", "wiesfknds5+*-8=", 0.0, true},
+		{"two_equal_signs", "1+3==", 0.0, true},
+		{"unary_minus_sign", "-3+5=", 2, false},
+		{"divide_by_zero_1", "55/0", 0.0, false},
+		{"divide_by_zero_2", "4/(10-2*5)", 0.0, true},
+	}
 
 	// act
-	act, err := Calc(s)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			act, err := Calc(tt.s)
 
-	// assert
-	if act != exp {
-		t.Errorf("Calc(%s) = %f; expected: %f", s, act, exp)
-	} else if err != nil {
-		t.Error(err)
+			// assert
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Calc() error: expected wantErr=%v, got err=%v", tt.wantErr, err)
+				return
+			}
+			if !tt.wantErr && act != tt.want {
+				t.Errorf("Calc() unexpected result: expected: %f, got: %f", tt.want, act)
+			}
+
+		})
 	}
 
 }

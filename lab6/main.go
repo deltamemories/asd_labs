1010e7e12e8db73b690a960e80966c3ae47d7862
Delta <maxenot123@gmail.com>
2025-10-09 23:13:40 +0200

    lab6: wip

---
+++ lab6/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"asd_labs/speedtest"
+	"fmt"
+)
+
+func main() {
+	arr := speedtest.NewRandomIntArray(10_000, 100)
+	fmt.Printf("Array: %v\n", arr)
+
+	sortedArr, time := speedtest.Speedtest(arr, Sort)
+	fmt.Printf("Sorted array: %v\n%s\n", sortedArr, time)
+}
+
+func Sort(arr []int) []int { // fix this
+	l := len(arr)
+	for i := 0; i < l-2; i++ {
+		for j := i + 1; i < l-1; j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
+}

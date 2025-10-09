c809eb8f4c1f520236334cce8d974da5a8a7e4d6
Delta <maxenot123@gmail.com>
2025-10-09 18:06:04 +0200

    add func NewRandomIntArray

--- speedtest/speedtest.go
+++ speedtest/speedtest.go
@@ -2,6 +2,7 @@ package speedtest
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -11,4 +12,12 @@ func Speedtest(arr []int, f sortingFunc) ([]int, string) {
 	t := time.Now()
 	newArr := f(arr)
 	return newArr, fmt.Sprintf("Time: %s", time.Since(t))
+}
+
+func NewRandomIntArray(l int, maxVal int) []int {
+	arr := make([]int, l)
+	for i := range arr {
+		arr[i] = int(rand.Intn(maxVal))
+	}
+	return arr
 }
